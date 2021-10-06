fb9ee9e02269a6bc813c8913ea9aa3f1a0e88d1e
Leonardo <leonardohsilva.dev@gmail.com>
2021-10-06 11:31:12 -0300

    chore: add twin.macro

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,22 @@
+import { css } from '@emotion/react'
+import tw from 'twin.macro'
 import { Layout } from 'systems/Core'
 
 export default function Home() {
-  return <Layout title="Events App">Hello World</Layout>
+  return (
+    <Layout title="Events App">
+      <div css={styles.root}>
+        <h1>Hello World</h1>
+      </div>
+    </Layout>
+  )
+}
+
+const styles = {
+  root: css`
+    ${tw`flex items-center justify-center width[100vw] height[100vh]`}
+    h1 {
+      ${tw`sm:(text-3xl) md:(text-5xl) hover:(text-primary-500) cursor-pointer`}
+    }
+  `,
 }
